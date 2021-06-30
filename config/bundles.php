e6f5677257af9a6adcda9cd322ef4a5ed5a7ab59
Patrick <patrick@vanberlin.com>
2021-06-30 19:50:00 +0200

    add CORS

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
