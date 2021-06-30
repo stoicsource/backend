a6f23aa49532cf7ef4a5a44b00d2fac0d65ca029
Patrick <patrick@vanberlin.com>
2021-07-01 00:39:57 +0200

    cleanup script

--- sql/remove empty editions.sql
+++ sql/remove empty editions.sql
@@ -3,7 +3,11 @@ FROM edition
 WHERE id NOT IN (SELECT DISTINCT edition_id FROM content);
 
 
-
 DELETE
 FROM work
 WHERE id NOT IN (SELECT DISTINCT work_id FROM edition);
+
+
+DELETE
+FROM toc_entry
+WHERE id NOT IN (SELECT DISTINCT toc_entry_id FROM content);
