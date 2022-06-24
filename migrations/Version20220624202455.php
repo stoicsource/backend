fe2774adb2547dd15f44d4e390775b9408028c70
Patrick <dev@stoicsource.com>
2022-06-24 22:30:06 +0200

    add fields to author: year alive, summary, more info URL

---
+++ migrations/Version20220624202455.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220624202455 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE author ADD years_alive VARCHAR(32) DEFAULT NULL, ADD summary LONGTEXT DEFAULT NULL, ADD more_info_url VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE author DROP years_alive, DROP summary, DROP more_info_url');
+    }
+}
